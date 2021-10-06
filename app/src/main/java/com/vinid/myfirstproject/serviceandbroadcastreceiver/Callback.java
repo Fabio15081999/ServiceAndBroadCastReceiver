8a2d4598da3a15dfbe5959c7fdb8ba1e342a97e8
Fabio <petertomx69@gmail.com>
2021-10-07 00:38:41 +0700

    Initial commit

---
+++ app/src/main/java/com/vinid/myfirstproject/serviceandbroadcastreceiver/Callback.java
@@ -0,0 +1,12 @@
+package com.vinid.myfirstproject.serviceandbroadcastreceiver;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by hungnm24 on 7/31/20
+ * Copyright (c) {2020} VinID. All rights reserved.
+ */
+
+public interface Callback {
+    void onDownloadFinish(String filePath);
+}
